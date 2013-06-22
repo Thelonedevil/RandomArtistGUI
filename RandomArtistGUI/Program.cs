74103de72e814eb8620b3623bb108cce14c723d5
Justin Wiblin <the_lone_devil@justin-wiblin.tk>
2013-06-22 21:05:59 +0100

    removed threading

--- RandomArtistGUI/Program.cs
+++ RandomArtistGUI/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 
@@ -13,7 +12,6 @@ namespace com.github.thelonedevil.RandomArtistGUI {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
         static void Main() {
             Load();
             Application.EnableVisualStyles();
